04cbfcfa3a2f6ee0e1cabb3ac091c93a462440e8
Eloy Vega <eloy.vega@wizeline.com>
2021-08-25 16:23:02 -0500

    steps

--- .github/scripts/handle_errors/index.js
+++ .github/scripts/handle_errors/index.js
@@ -6,7 +6,7 @@ console.log(process.env.OWNER);
 console.log(process.env.REPO);
 console.log(process.env.RUN_ID);
 
-const getWorkflowRunDetails = async ({ repo, owner, runId }) => {
+const getFailedStep = async (repo, owner, runId) => {
   try {
     const res = await octokit.rest.actions.listJobsForWorkflowRun({
       owner,
@@ -17,16 +17,26 @@ const getWorkflowRunDetails = async ({ repo, owner, runId }) => {
     console.log(workflowRunDetails);
     for (const jobs of workflowRunDetails.jobs) {
       for (const step of jobs.steps) {
-        console.log(step);
+        if (step.conclusion === "failure") {
+          return step.name;
+        }
       }
     }
   } catch (error) {
     console.log(error);
+    throw "Fail at getting workflow run";
   }
 };
 
-getWorkflowRunDetails({
+const main = async ({ repo, owner, runId, stepAllowlist }) => {
+  const failedStep = getFailedStep(repo, owner, runId);
+  console.log(failedStep);
+  console.log(stepAllowlist);
+};
+
+main({
   repo: process.env.REPO,
   owner: process.env.OWNER,
   runId: process.env.RUN_ID,
+  stepAllowlist: process.env.STEP_ALLOWLIST.split(","),
 });
