5c69ca82a12012808434f2ba241de7e427483624
Eloy Vega <eloy.vega@wizeline.com>
2021-08-25 15:48:19 -0500

    js

--- .github/scripts/handle_errors/index.js
+++ .github/scripts/handle_errors/index.js
@@ -1,15 +1,21 @@
 const { Octokit } = require("octokit");
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
+console.log(process.env.GITHUB_TOKEN);
+console.log(process.env.OWNER);
+console.log(process.env.REPO);
+
 const getWorkflowRunDetails = async ({ repo, owner, runId }) => {
   const res = await octokit.rest.actions.listJobsForWorkflowRun({
     owner,
     repo,
     run_id: runId,
   });
-  return res.data;
+  console.log(res.data);
 };
 
 getWorkflowRunDetails({
-  repo: "",
+  repo: process.env.REPO,
+  owner: process.env.OWNER,
+  run_id: process.env.RUN_ID,
 });
